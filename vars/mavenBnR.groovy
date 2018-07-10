dd370d09d8ed0f70de06c74fcdd345d7591b8e7a
Amit Kumar <akumar105@sapient.com>
2018-07-10 15:07:19 +0100

    why node?

--- vars/mavenBnR.groovy
+++ vars/mavenBnR.groovy
@@ -1,34 +1,33 @@
 #!/usr/bin/env groovy
 
 def call(String mavenName = 'M3'){
-    node {
-        stage('Tests'){
-            withMaven(
-                maven: "${mavenName}"
-            ){
-                sh "mvn test"
-            }
-        }
-        stage('Sonar'){
-            withMaven(
-                maven: "${mavenName}"
-            ){
-                sh "mvn sonar:sonar"
-            }
-        }
-        stage('Package'){
-            withMaven(
-                maven: "${mavenName}"
-            ){
-                sh "mvn -B package"
-            }
-        }
-        stage('Push to Nexus'){
-            withMaven(
-                maven: "${mavenName}"
-            ){
-                sh "mvn deploy"
-            }
+    stage('Tests'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            sh "mvn test"
         }
     }
+    stage('Sonar'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            sh "mvn sonar:sonar"
+        }
+    }
+    stage('Package'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            sh "mvn -B package"
+        }
+    }
+    stage('Push to Nexus'){
+        withMaven(
+            maven: "${mavenName}"
+        ){
+            sh "mvn deploy"
+        }
+    }
+
 }
